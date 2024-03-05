b053924563b4de8fa7e4921649ab8283e6b7b5a2
KimSeYun <ksy642@dcarrick.co.kr>
2024-03-05 13:15:25 +0900

    StringValues 추가

---
+++ UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/DataBase/StringValues.cs
@@ -0,0 +1,17 @@
+public static class StringValues
+{
+    public static class Text
+    {
+        public const string test1 = "test1";
+    }
+
+
+    public const string test2 = "test2";
+    public const string test3 = "test3";
+
+    // ¾À
+    public static class Scene
+    {
+        public const string login = "1_Login";
+    }
+}
