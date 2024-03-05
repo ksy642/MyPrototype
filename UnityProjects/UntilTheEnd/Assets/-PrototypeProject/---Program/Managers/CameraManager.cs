28d6c49f15a89d3beabb88053b7094ca4113a8b1
KimSeYun <ksy642@dcarrick.co.kr>
2024-03-05 13:19:03 +0900

    CameraManager, UIManager 추가

---
+++ UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/Managers/CameraManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera thisCamera;
+
+    private float _scrollSpeed = 10.0f;
+    private float _initialFieldOfView;
+
+    void Start()
+    {
+        // √ ±‚ fieldOfView ∞™¿ª ¿˙¿Â
+        _initialFieldOfView = thisCamera.fieldOfView;
+    }
+
+    void Update()
+    {
+        float scrollWheel = -Input.GetAxis("Mouse ScrollWheel") * _scrollSpeed;
+
+        // ∏∂øÏΩ∫ »Ÿ ≈¨∏Ø, √ ±‚∞™ : 60
+        if (Input.GetMouseButtonDown(2))
+        {
+            thisCamera.fieldOfView = _initialFieldOfView;
+        }
+        else
+        {
+            // √÷¥Î ¡‹ ¿Œ : 20
+            if (thisCamera.fieldOfView <= 20.0f && scrollWheel < 0)
+            {
+                thisCamera.fieldOfView = 20.0f;
+            }
+
+            // √÷¥Î ¡‹ æ∆øÙ : 100
+            else if (thisCamera.fieldOfView >= 100.0f && scrollWheel > 0)
+            {
+                thisCamera.fieldOfView = 100.0f;
+            }
+
+            // "¡‹¿Œ ¡‹æ∆øÙ" ∞°¥…
+            else
+            {
+                thisCamera.fieldOfView += scrollWheel;
+            }
+        }
+    }
+}
---
+++ UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/Managers/UIManager.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : DontDestroySingleton<UIManager>
+{
+    public TMP_InputField nicknameinput;
+    public GameObject panel_ESC;
+
+    private UserSettings _userSettings;
+
+    public void OnClickNickname()
+    {
+        string nickname = nicknameinput.text;
+
+        if (string.IsNullOrWhiteSpace(nickname))
+        {
+            nickname = "UserName " + Random.Range(1, 10000);
+        }
+
+        PlayerPrefs.SetString("Nickname", nickname);
+        _userSettings.userNickName = nickname;
+        //PhotonNetwork.NickName = nickname;
+        Debug.Log(" 닉네임  : " + nickname + " 저장 완료");
+    }
+
+    // MouseLook.cs Update 동작
+    public bool TogglePanelESC(bool menuESC = false)
+    {
+        if (!menuESC) // 기본값 false 즉, 여기부터 시작
+        {
+            panel_ESC.gameObject.SetActive(false);
+            return true;
+        }
+        else // true
+        {
+            panel_ESC.gameObject.SetActive(true);
+            return false;
+        }
+    }
+
+    // ESC 메뉴에서 나가기 버튼 눌렀을 때
+    public void OnToLobby()
+    {
+        // 세이브 로드 설정해야함 !!
+        SceneManager.LoadScene("1_Lobby");
+    }
+}
