b52ab9ed6fa8f8ddbac958bff45b4a58c632bf45
SeYun <ksy642@naver.com>
2024-04-24 00:20:53 +0900

    Preload 돌아갈 때 PhotonNetwork 체크

--- UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/0_Preload/Preload.cs
+++ UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/0_Preload/Preload.cs
@@ -1,13 +1,17 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class Preload : MonoBehaviour
 {
-    // 매니저들 체크
-    // 없으면 생성시켜주는 식으로
-    // 있으면 그대로 진행
-
     void Start()
     {
-        SceneManager.instance.LoadScene("1_Login");
+        if (PhotonNetwork.IsConnected)
+        {
+            NetworkManager.instance.RoomNotFull();
+        }
+        else
+        {
+            SceneManager.instance.LoadScene("1_Login");
+        }
     }
 }
