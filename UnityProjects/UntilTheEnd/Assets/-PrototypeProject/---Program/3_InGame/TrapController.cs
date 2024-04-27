f80fe25a77baeb9f1a4154b6ed7dd0aa580630a4
SeYun <ksy642@naver.com>
2024-04-27 23:07:41 +0900

    Trap, NPC Controller 스크립트만 만들어둠 제작 들어갈 예정

--- UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/3_InGame/TrapController.cs
+++ UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/3_InGame/TrapController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap : MonoBehaviour
+public class TrapController : MonoBehaviour
 {
     bool hasTriggered = false;
 
