d8912910e8bd59417c5a813aacbd7a71bb1ca9ba
SeYun <ksy642@naver.com>
2024-04-28 23:37:10 +0900

    [시네카메라] 플레이어 연결o, 특정 트리거 밟으면 = 카메라 변경 그리고 다시 돌아오게 Test용 확인ok

--- UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/3_InGame/TrapController.cs
+++ UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/3_InGame/TrapController.cs
@@ -1,22 +1,42 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using Photon.Realtime;
 
 public class TrapController : MonoBehaviour
 {
+    public CinemachineVirtualCamera cameraPlayer; // 11번 카메라
+    public CinemachineVirtualCamera cameraTest1; // 9번 카메라
+
     bool hasTriggered = false;
 
     void OnTriggerEnter(Collider other)
     {
-        // 플레이어가 특정구간 밟았을 때 동작하는 코드
+        // 플레이어가 특정 구간 밟았을 때 동작하는 코드
         if (other.CompareTag("Player") && !hasTriggered)
         {
-            // 시네머신 같은거 넣어서 카메라 돌리고 좀비군단 같은거 소환하게 하는거 살짝 보여주는거 괜찮다고 생각됨
-
+            // 플레이어가 트리거를 밟은 상태일 때 실행할 코드
             Debug.Log("플레이어가 트리거를 밟은 상태");
 
+            ChangeView();
 
-            hasTriggered = true;
+            hasTriggered = true; // 트리거가 한 번만 발동하도록 설정
         }
+    }
+
+    void ChangeView()
+    {
+        cameraPlayer.gameObject.SetActive(false);
+        cameraTest1.gameObject.SetActive(true);
+
+        StartCoroutine(ChangeCameraForSeconds()); // 11번 카메라로 변경 후 5초 후 9번 카메라로 변경
+    }
+
+    IEnumerator ChangeCameraForSeconds()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        cameraTest1.gameObject.SetActive(false);
+        cameraPlayer.gameObject.SetActive(true);
     }
 }
