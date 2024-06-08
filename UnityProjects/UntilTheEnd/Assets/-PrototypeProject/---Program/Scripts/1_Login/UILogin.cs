191ee8c51413571f6d3afea381f15e3aefbef658
SeYun <ksy642@naver.com>
2024-06-08 18:15:15 +0900

    로그인씬에서 인게임들어갈 때 로딩창 넘어가게 설정 (먼저 로컬개발부터 시작)

--- UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/Scripts/1_Login/UILogin.cs
+++ UnityProjects/UntilTheEnd/Assets/-PrototypeProject/---Program/Scripts/1_Login/UILogin.cs
@@ -59,11 +59,11 @@ namespace UntilTheEnd
         {
             // 방만드는식으로 진행이 될 예정...허나 먼저 포톤연결해서 들어가는식으로 진행을 하도록 하자...
             NetworkManager.instance.LoginServer(); // 다이렉트로 로비까지 진입ok
-                                                   // 여기서 씬 넘기거나 방 같은거 만드는쪽으로 Canvas 만들어서 구성하고 거기서 Ready Go 하면 씬 넘어가게 설정가능
+            // 여기서 씬 넘기거나 방 같은거 만드는쪽으로 Canvas 만들어서 구성하고 거기서 Ready Go 하면 씬 넘어가게 설정가능
 
         }
 
-        // Main Game Button Single
+        // Main Game Button Single :: 먼저 싱글부터 개발
         public void Panel_MainGameSingle()
         {
             StartCoroutine(WaitTest());
@@ -73,8 +73,7 @@ namespace UntilTheEnd
         {
             yield return new WaitForSeconds(2.0f);
 
-            //SceneManager.instance.LoadScene("2_Lobby");
-            SceneManager.instance.LoadScene("3_InGame");
+            Loading.LoadScene(StringValues.Scene.inGame);
         }
         #endregion
     }
